c34c470bae847a3a4ee7a64ae143c8ba9f340f28
hakynbayo <96801721+hakynbayo@users.noreply.github.com>
2022-06-29 00:36:06 +0100

    initial commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,18 @@
 function checkPrime(num) {
-  
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-module.exports = checkPrime
+console.log(checkPrime(8));
+console.log(checkPrime(13));
+console.log(checkPrime(127));
+console.log(checkPrime(150));
+module.exports = checkPrime;
